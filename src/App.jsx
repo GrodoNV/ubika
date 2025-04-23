83eba8b0f1940cea0f0e1eb51c9e2e10eaa89971
GrodoNV <grodrigonvargas@gmail.com>
2025-04-22 21:07:09 -0500

    🎉 init: estructura base de Ubika con Vite y Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+// src/App.jsx
+export default function App() {
+    return (
+      <div className="text-center mt-10">
+        <h1 className="text-3xl font-bold">Ubika</h1>
+        <p className="text-gray-600">Tu conexión con el transporte móvil en La Paz</p>
+      </div>
+    );
+  }
+  
